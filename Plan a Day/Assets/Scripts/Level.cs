3f24bc1701f27a0a25d8bd65b6b75730ac0e08bf
Subash <it.subashaa@gmail.com>
2022-08-14 15:16:37 +0530

    QuestionPrefab

--- Plan a Day/Assets/Scripts/Level.cs
+++ Plan a Day/Assets/Scripts/Level.cs
@@ -1,6 +1,33 @@
 using UnityEngine;
+using UnityEngine.UI;
 
 public class Level : MonoBehaviour
 {
     public Round[] RoundList;
+    [SerializeField] Transform Panel;
+
+    private LevelManager manager;
+
+
+    private void Awake()
+    {
+        manager = GetComponentInParent<LevelManager>();
+    }
+
+    private void Start()
+    {
+        for(int i = 0; i < RoundList.Length; i++)
+        {
+            for(int j = 0; j < RoundList[i].Question.Length; j++)
+            {
+                GameObject question = Instantiate(manager.GetQuestionPrefab(), Panel);
+                question.name = "Question: " + (j + 1);
+                
+                if(question.TryGetComponent<Text>(out Text questText))
+                {
+                    questText.text = RoundList[i].Question[j];
+                }
+            }
+        }
+    }
 }
